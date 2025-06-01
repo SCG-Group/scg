bdefcbe5138a3a4643488aefb492403c77211d34
fraz <fraz@o2.pl>
2025-06-01 19:51:07 +0200

    theme setup

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+// Legacy config needed for VSCode extension.
+module.exports = {
+	extends: [ 'plugin:@wordpress/eslint-plugin/recommended' ],
+	env: {
+		jquery: true,
+		browser: true,
+	},
+};
---
+++ stylelint.config.js
@@ -0,0 +1,14 @@
+// Legacy config needed for VSCode extension.
+module.exports = {
+	extends: [ '@wordpress/stylelint-config/scss-stylistic' ],
+	rules: {
+		'selector-class-pattern': null,
+		'selector-id-pattern': null,
+	},
+	overrides: [
+		{
+			files: [ '**/*.scss' ],
+			customSyntax: 'postcss-scss',
+		},
+	],
+};
