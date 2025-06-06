221e02f52df342008ebccd6fe6277ec08642bc2a
fraz <fraz@o2.pl>
2025-06-06 16:41:17 +0200

    update folder structure and build scripts

---
+++ functions.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Theme functions and definitions
+ *
+ * @package WordPress
+ * @subpackage SCG
+ */
+
+namespace SCG;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+require_once __DIR__ . '/includes/assets.php';
+
+define( 'SCG_THEME_VERSION', '1.0.0' );
+
+/**
+ * Enqueue theme styles and scripts.
+ */
+add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_assets' );
---
+++ includes/assets.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Theme assets
+ *
+ * @package WordPress
+ * @subpackage SCG
+ */
+
+namespace SCG;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * Enqueue theme styles and scripts.
+ */
+function enqueue_assets() {
+	// Styles.
+	$styles_uri      = '/build/scg-styles.css';
+	$styles_dep_path = '/build/scg-styles.asset.php';
+	$styles          = include get_theme_file_path( $styles_dep_path );
+
+	if ( $styles ) {
+		wp_enqueue_style(
+			'scg-theme-styles',
+			get_theme_file_uri( $styles_uri ),
+			$styles['dependencies'] ?? array(),
+			$styles['version'] ?? SCG_THEME_VERSION
+		);
+	}
+
+	// Scripts.
+	$script_uri      = '/build/scg-scripts.js';
+	$script_dep_path = '/build/scg-scripts.asset.php';
+	$script          = include get_theme_file_path( $script_dep_path );
+
+	if ( $script ) {
+		wp_enqueue_script_module(
+			'scg-theme-scripts',
+			get_theme_file_uri( $script_uri ),
+			$script['dependencies'] ?? array(),
+			$script['version'] ?? SCG_THEME_VERSION
+		);
+	}
+}
