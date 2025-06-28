b6baa955e1e8b5325105cdb151afdd1b8ab6a478
fraz <fraz@o2.pl>
2025-06-28 13:09:12 +0200

    style bottom footer section

--- patterns/footer.php
+++ patterns/footer.php
@@ -31,14 +31,18 @@
 	<!-- /wp:columns -->
 	<!-- wp:group {"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"},"className":"scg-footer__bottom","fontSize":"14"} -->
 	<div class="wp-block-group scg-site-footer__bottom has-14-font-size">
-		<!-- wp:scg/logo {"isLink":false, "className": "is-style-secondary"} /-->
-		<!-- wp:paragraph -->
-		<p>&copy; <?php echo esc_html( sprintf( '%1$s %2$s %3$s', __( 'Copyright', 'scg' ), gmdate( 'Y' ), get_bloginfo( 'name' ) ) ); ?></p>
-		<!-- /wp:paragraph -->
-		<!-- wp:navigation {"overlayMenu":"never"} -->
-			<!-- wp:navigation-link {"label":"Privacy Policy","type":"page","url":"#","kind":"post-type"} /-->
-			<!-- wp:navigation-link {"label":"Terms and conditions","type":"page","url":"#","kind":"post-type"} /-->
-		<!-- /wp:navigation -->
+		<!-- wp:group {"className":"scg-site-footer__bottom","layout":{"type":"flex","flexWrap":"wrap"}} -->
+		<div class="wp-block-group scg-site-footer__bottom">
+			<!-- wp:scg/logo {"isLink":false, "className": "is-style-secondary"} /-->
+			<!-- wp:paragraph -->
+			<p>&copy; <?php echo esc_html( sprintf( '%1$s %2$s %3$s', __( 'Copyright', 'scg' ), gmdate( 'Y' ), get_bloginfo( 'name' ) ) ); ?></p>
+			<!-- /wp:paragraph -->
+			<!-- wp:navigation {"overlayMenu":"never"} -->
+				<!-- wp:navigation-link {"label":"Privacy Policy","type":"page","url":"#","kind":"post-type"} /-->
+				<!-- wp:navigation-link {"label":"Terms and conditions","type":"page","url":"#","kind":"post-type"} /-->
+			<!-- /wp:navigation -->
+		</div>
+		<!-- /wp:group -->
 		<!-- wp:template-part {"slug":"social-media-links","theme":"scg"} /-->
 	</div>
 	<!-- /wp:group -->
