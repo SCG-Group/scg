13db3a90c07c42873750fc6dba47a1f369e5bc2f
fraz <fraz@o2.pl>
2025-07-08 17:22:15 +0200

    add stats section pattern

--- patterns/splash.php
+++ patterns/splash.php
@@ -10,12 +10,12 @@
  */
 
 ?>
-<!-- wp:cover {"hasParallax":true,"dimRatio":20,"overlayColor":"#000000","customOverlayColor":"#000000","isUserOverlayColor":true,"minHeight":100,"minHeightUnit":"vh","contentPosition":"center center","tagName":"section","sizeSlug":"large","metadata":{"categories":["featured"],"patternName":"scg/section-splash","name":"Section Splash"},"align":"full","className":"scg-featured","layout":{"type":"constrained"}} -->
+<!-- wp:cover {"hasParallax":true,"dimRatio":20,"overlayColor":"#000000","customOverlayColor":"#000000","isUserOverlayColor":true,"minHeight":100,"minHeightUnit":"vh","contentPosition":"center center","tagName":"section","sizeSlug":"large","metadata":{"categories":["featured"],"patternName":"scg/page-splash","name":"Page Splash"},"align":"full","className":"scg-featured","layout":{"type":"constrained"}} -->
 <section class="wp-block-cover alignfull has-parallax has-custom-content-position scg-featured" style="min-height:100vh">
 	<span aria-hidden="true" class="wp-block-cover__background has-000000-background-color has-background-dim-20 has-background-dim"></span>
 	<div class="wp-block-cover__inner-container">
 		<!-- wp:heading {"level": 1, "fontSize":"160"} -->
-		<h1 class="wp-block-heading has-160-font-size">A partner <br />you can trust</h2>
+		<h1 class="wp-block-heading has-160-font-size"><mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-accent-color">A partner</mark> <br />you can trust</h2>
 		<!-- /wp:heading -->
 	</div>
 </section>
---
+++ patterns/stats.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Title: Stats section
+ * Slug: scg/stats-section
+ * Categories: featured
+ *
+ * @package WordPress
+ * @subpackage SCG
+ */
+
+?>
+<!-- wp:group {"tagName":"section","metadata":{"name":"Stats"},"layout":{"type":"grid","minimumColumnWidth":null,"columnCount":2}} -->
+<section class="wp-block-group">
+	<!-- wp:group {"style":{"layout":{"columnSpan":1},"spacing":{"blockGap":"var:preset|spacing|16"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+	<div class="wp-block-group">
+		<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|accent"}}}},"textColor":"accent","fontSize":"128","fontFamily":"headings"} -->
+		<p class="has-accent-color has-text-color has-link-color has-headings-font-family has-128-font-size">122g</p>
+		<!-- /wp:paragraph -->
+
+		<!-- wp:paragraph -->
+		<p>CO₂/km emissions</p>
+		<!-- /wp:paragraph -->
+	</div>
+	<!-- /wp:group -->
+
+	<!-- wp:group {"style":{"layout":{"selfStretch":"fill","flexSize":null},"spacing":{"blockGap":"var:preset|spacing|16"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+	<div class="wp-block-group">
+		<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|accent"}}}},"textColor":"accent","fontSize":"128","fontFamily":"headings"} -->
+		<p class="has-accent-color has-text-color has-link-color has-headings-font-family has-128-font-size">+1,5K</p>
+		<!-- /wp:paragraph -->
+
+		<!-- wp:paragraph -->
+		<p>Orders per month</p>
+		<!-- /wp:paragraph -->
+	</div>
+	<!-- /wp:group -->
+
+	<!-- wp:group {"style":{"layout":{"selfStretch":"fill","flexSize":null},"spacing":{"blockGap":"var:preset|spacing|16"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+	<div class="wp-block-group">
+		<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|accent"}}}},"textColor":"accent","fontSize":"128","fontFamily":"headings"} -->
+		<p class="has-accent-color has-text-color has-link-color has-headings-font-family has-128-font-size">+1mln</p>
+		<!-- /wp:paragraph -->
+
+		<!-- wp:paragraph -->
+		<p>km/month</p>
+		<!-- /wp:paragraph -->
+	</div>
+	<!-- /wp:group -->
+
+	<!-- wp:group {"style":{"layout":{"selfStretch":"fill","flexSize":null},"spacing":{"blockGap":"var:preset|spacing|16"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+	<div class="wp-block-group">
+		<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|accent"}}}},"textColor":"accent","fontSize":"128","fontFamily":"headings"} -->
+		<p class="has-accent-color has-text-color has-link-color has-headings-font-family has-128-font-size">+100</p>
+		<!-- /wp:paragraph -->
+
+		<!-- wp:paragraph -->
+		<p>Vehicles</p>
+		<!-- /wp:paragraph -->
+	</div>
+	<!-- /wp:group -->
+</section>
+<!-- /wp:group -->
