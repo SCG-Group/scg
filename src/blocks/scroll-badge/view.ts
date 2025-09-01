43a6a468de0107d054a5e13d74674c9d77fc5e52
fraz <fraz@o2.pl>
2025-09-01 16:54:59 +0200

    add reduced motion support to scroll badge block

--- src/blocks/scroll-badge/view.ts
+++ src/blocks/scroll-badge/view.ts
@@ -1,24 +1,28 @@
 import { getElement, store } from '@wordpress/interactivity';
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
+import { REDUCED_MOTION_QUERY } from '../../scripts/constants.ts';
 
 store( 'scg/scroll-badge', {
 	callbacks: {
 		initScrollBadge: () => {
 			const element = getElement().ref;
+			const animation = gsap.matchMedia();
 
 			gsap.registerPlugin( ScrollTrigger );
 
-			gsap.to( '#scg-scroll-badge-frame', {
-				scrollTrigger: {
-					trigger: element,
-					scrub: true,
-					start: 'bottom bottom',
-					end: 'bottom top',
-				},
-				rotation: 720,
-				transformOrigin: 'center',
-				ease: 'power3.in',
+			animation.add( REDUCED_MOTION_QUERY, () => {
+				gsap.to( '#scg-scroll-badge-frame', {
+					scrollTrigger: {
+						trigger: element,
+						scrub: true,
+						start: 'bottom bottom',
+						end: 'bottom top',
+					},
+					rotation: 720,
+					transformOrigin: 'center',
+					ease: 'power3.in',
+				} );
 			} );
 		},
 	},
