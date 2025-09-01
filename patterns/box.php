b9b78845632f39196e2caae58a8738449d19f0e7
fraz <fraz@o2.pl>
2025-09-01 19:42:07 +0200

    revisit box pattern

--- patterns/box.php
+++ patterns/box.php
@@ -9,15 +9,17 @@
  */
 
 ?>
-<!-- wp:group {"metadata":{"name":"Box"},"className":"scg-border scg-border\u002d\u002daccent","style":{"spacing":{"padding":{"top":"var:preset|spacing|32","bottom":"var:preset|spacing|32","left":"var:preset|spacing|32","right":"var:preset|spacing|32"}}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"center"}} -->
-<div class="wp-block-group scg-border scg-border--accent" style="padding-top:var(--wp--preset--spacing--32);padding-right:var(--wp--preset--spacing--32);padding-bottom:var(--wp--preset--spacing--32);padding-left:var(--wp--preset--spacing--32)"><!-- wp:safe-svg/svg-icon {"svgURL":"https://scg.local/wp-content/uploads/2025/07/Frame-6.svg","imageID":552,"imageWidth":96,"imageHeight":97,"dimensionWidth":96,"dimensionHeight":96,"imageSizes":{"full":{"height":97,"width":96,"url":"https://scg.local/wp-content/uploads/2025/07/Frame-6.svg","orientation":"portrait"},"medium":{"height":"300","width":"300","url":"https://scg.local/wp-content/uploads/2025/07/Frame-6.svg","orientation":"portrait"},"thumbnail":{"height":"150","width":"150","url":"https://scg.local/wp-content/uploads/2025/07/Frame-6.svg","orientation":"portrait"}}} /-->
+<!-- wp:group {"metadata":{"name":"Box"},"className":"scg-box","style":{"spacing":{"padding":{"top":"var:preset|spacing|32","bottom":"var:preset|spacing|32","left":"var:preset|spacing|32","right":"var:preset|spacing|32"}}},"layout":{"type":"flex","flexWrap":"wrap"}} -->
+<div class="wp-block-group scg-box" style="padding-top:var(--wp--preset--spacing--32);padding-right:var(--wp--preset--spacing--32);padding-bottom:var(--wp--preset--spacing--32);padding-left:var(--wp--preset--spacing--32)"><!-- wp:image {"id":552,"sizeSlug":"large","linkDestination":"none"} -->
+	<figure class="wp-block-image size-large"><img src="https://scg.local/wp-content/uploads/2025/07/Frame-6.svg" alt="" class="wp-image-552" /></figure>
+	<!-- /wp:image -->
 
 	<!-- wp:paragraph {"placeholder":"Content…","style":{"typography":{"fontWeight":"700","fontStyle":"normal"},"layout":{"selfStretch":"fill","flexSize":null}},"fontSize":"32","fontFamily":"body"} -->
 	<p class="has-body-font-family has-32-font-size" style="font-style:normal;font-weight:700">Distribution<br>and Supply Chain</p>
 	<!-- /wp:paragraph -->
 
 	<!-- wp:paragraph {"align":"left","style":{"layout":{"selfStretch":"fit","flexSize":null}}} -->
-	<p class="has-text-align-left">From last-mile deliveries to full supply chain management, we help businesses reduce costs and improve efficiency. </p>
+	<p class="has-text-align-left">From last-mile deliveries to full supply chain management, we help businesses reduce costs and improve efficiency.&nbsp;</p>
 	<!-- /wp:paragraph -->
 </div>
 <!-- /wp:group -->
