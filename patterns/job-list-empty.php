f2cbf32fefde74d16c292c914cb1dcdeac716b8c
fraz <fraz@o2.pl>
2025-09-07 11:38:57 +0200

    add pattern for empty job list

---
+++ patterns/job-list-empty.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Title: Empty Job List
+ * Slug: scg/job-list-empty
+ *
+ * @package WordPress
+ * @subpackage SCG
+ */
+
+?>
+
+<!-- wp:paragraph {"metadata":{"name":"Empty Job List"}} -->
+<p>We currently don’t have any open positions.<br>If you’d like to be considered for future opportunities, please send us your CV or contact details at <a href="mailto:contact@scgtransport.com"><strong>contact@scgtransport.com</strong></a></p>
+<!-- /wp:paragraph -->
